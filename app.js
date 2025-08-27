6885f93a26acfe2058f673b899c150dcbbe41326
nitin <nitindagar8920@gmail.com>
2025-08-27 12:04:47 +0530

    feat: add user reviews and ratings functionality

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const listingSchema = require("./schema.js");
+const {listingSchema, reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";   
 
@@ -44,6 +45,17 @@ const validateListing = (req, res, next)=>{
     }
 }
 
+const validateReview = (req, res, next) =>{
+    let {error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    else{
+        next();
+    }
+}
+
 //Index Route
 app.get("/listings", wrapAsync(async (req, res)=>{
     let allListings = await Listing.find({});
@@ -59,7 +71,7 @@ app.get("/listings/new", (req, res)=>{
 //Show Route
 app.get("/listings/:id", wrapAsync(async (req, res)=>{
     let { id } = req.params;
-    let list = await Listing.findById(id);
+    const list = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", {list});
 }));
 
@@ -97,6 +109,30 @@ app.delete("/listings/:id", wrapAsync(async (req, res)=>{
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
 }));
+
+//Review post route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//Review Delete Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let {id, reviewId} = req.params;
+
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}));
+
 
 app.all(/.*/, (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main().then(()=>{
@@ -35,6 +36,18 @@ const listingSchema = Schema({
     country: {
         type: String,
     },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        }
+    ]
+});
+
+listingSchema.post("findOneAndDelete", async(listing) => {
+    if(listing){
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
